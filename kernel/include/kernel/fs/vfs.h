a2c50cfa21484d63a5116e5a82ef756189263c82
RMuskovets <rmuskovets@gmail.com>
2019-10-11 17:06:18 +0300

    fs structure

--- kernel/include/kernel/fs/vfs.h
+++ kernel/include/kernel/fs/vfs.h
@@ -3,20 +3,27 @@
 
 #include <disk.h>
 
+typedef struct file_data {
+	uint8_t is_directory;
+	uint16_t files_num;
+	uint64_t ptr;
+} __attribute__((packed)); // 12b
+
 typedef struct file {
 	uint32_t    vfs_id;
 	uint64_t    file_id;
 	uint64_t    parent_id;
 	uint32_t    name_len;
-	char       *name;
-	uint64_t    content_ptr;
-} file_t;
+	//char       *name;
+	uint64_t    name_ptr;
+	uint64_t    data_ptr;
+} file_t __attribute__((packed)); // 40b 
 
 typedef struct vfs {
-	//disk_t *d;
-	
 	uint32_t vfs_id;
-} vfs_t;
+	uint16_t files_num;
+	uint64_t ptr;
+} vfs_t __attribute__((packed)); // 14b
 
 vfs_t *vfs(disk_t *);
 void vfs_free(vfs_t *);
